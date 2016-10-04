bb9f994bf611661196a421bdda33faafe514ecd3
Johannes Zillmann <jz@datameer.com>
2016-10-04 02:16:24 +0200

    something working

--- src/javascript/components/BenchmarkList.jsx
+++ src/javascript/components/BenchmarkList.jsx
@@ -3,6 +3,7 @@ import Nav from 'react-bootstrap/lib/Nav'
 import NavItem from 'react-bootstrap/lib/NavItem'
 import AutoAffix from 'react-overlays/lib/AutoAffix';
 import BenchmarkReport from './BenchmarkReport.jsx';
+import Waypoint from 'react-waypoint';
 
 function groupBy(xs, key) {
     return xs.reduce(function(rv, x) {
@@ -34,49 +35,95 @@ export default class BenchmarkList extends Component {
         benchmarks: React.PropTypes.array.isRequired,
     };
 
-    state = {
-        activeNavItemHref: null
+    constructor(props) {
+        super(props);
+
+        const benchmarks = this.props.benchmarks;
+        // console.debug(benchmarks.length);
+
+        // const groupByPackage = groupBy(benchmarks, benchmarkToPackage);
+        // console.debug(groupByPackage.length);
+        // groupByPackage.forEach(function(element) {
+        //     console.debug(element)
+        // });
+
+        const groupByClassName = groupBy(benchmarks, benchmarkToClassName);
+        // console.debug(groupByClassName.length);
+        // groupByClassName.forEach(function(element) {
+        //     console.debug(element)
+        // });
+
+        var selectedBenchmarkClassName;
+        console.debug("_-_" + window.location);
+        const {hash} = window.location;
+        console.debug(hash == '');
+        if (hash == null || hash == '') {
+            selectedBenchmarkClassName = groupByClassName[0].key
+        } else {
+            selectedBenchmarkClassName = hash.substring(1)
+        }
+
+        this.state = {
+            currentBenchmarkClassName: selectedBenchmarkClassName,
+            benchmarksGroupedByClassName: groupByClassName
+        };
+    }
+
+
+    setCurrentBenchmark(benchmarkClassName) {
+        console.debug("setCurrentBenchmark:" + benchmarkClassName);
+        // setTimeout(() => {
+        this.setState({
+            currentBenchmarkClassName: benchmarkClassName
+        });
+    // });
+    }
+
+    renderWaypoint(benchmarkClassName) {
+        return (
+            <Waypoint
+                      onEnter={ ({previousPosition}) => (
+                                previousPosition === Waypoint.above && this.setCurrentBenchmark(benchmarkClassName)
+                                ) }
+                      onLeave={ ({currentPosition}) => (
+                                currentPosition === Waypoint.above && this.setCurrentBenchmark(benchmarkClassName)
+                                ) }
+                      topOffset={ 10 }
+                      bottomOffset={ -10 } />
+            );
     }
 
     handleNavItemSelect(key, e) {
         window.location = e.target.href;
+        const {hash} = window.location;
+    // this.setCurrentBenchmark(hash.substring(0))
     }
 
     render() {
+        console.debug("render");
 
-        const benchmarks = this.props.benchmarks;
-        console.debug(benchmarks.length);
-
-        const groupByPackage = groupBy(benchmarks, benchmarkToPackage);
-        console.debug(groupByPackage.length);
-        groupByPackage.forEach(function(element) {
-            console.debug(element)
-        });
-
-        const groupByClassName = groupBy(benchmarks, benchmarkToClassName);
-        console.debug(groupByClassName.length);
-        groupByClassName.forEach(function(element) {
-            console.debug(element)
-        });
 
         return (
             <div>
               <div ref="main" className="container bs-docs-container">
                 <div className="row">
                   <p>
-                    { groupByClassName.length } different Benchmark classes detected!
+                    { this.state.benchmarksGroupedByClassName.length } different Benchmark classes detected!
                   </p>
                   <div className="col-md-10" role="main">
-                    { groupByClassName.map((element) => <BenchmarkReport key={ element.key } name={ element.key } methodBenchmarks={ element.values } />
+                    { this.state.benchmarksGroupedByClassName.map((element) => <div key={ element.key }>
+                                                                                 { this.renderWaypoint(element.key) }
+                                                                                 <BenchmarkReport name={ element.key } methodBenchmarks={ element.values } />
+                                                                               </div>
                       ) }
                   </div>
                   <div className="col-md-2 bs-docs-sidebar-holder">
                     <AutoAffix viewportOffsetTop={ 15 } container={ this }>
                       <div className="bs-docs-sidebar hidden-print" role="complementary">
-                        <Nav className="bs-docs-sidenav" activeHref={ this.state.activeNavItemHref } onSelect={ this.handleNavItemSelect }>
-                          { groupByClassName.map((element) => <NavItem href={ "#" + element.key } key={ element.key }>
-                                                                { element.key }
-                                                              </NavItem>
+                        <Nav className="bs-docs-sidenav" activeHref={ "#" + this.state.currentBenchmarkClassName } onSelect={ this.handleNavItemSelect }>
+                          { this.state.benchmarksGroupedByClassName.map((element) => <NavItem href={ "#" + element.key } key={ element.key }>
+                                                                                       { element.key }
+                                                                                     </NavItem>
                             ) }
                         </Nav>
                       </div>
